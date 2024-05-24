a54f509f9d5a093fcbf159b32ac61fdab2ec5d89
FADOD1 <110921479+FADOD1@users.noreply.github.com>
2024-05-24 19:32:24 -0300

    Update enviarEmbed.js

--- BOT-BASE-BYFADOD1/commands/utility/enviarEmbed.js
+++ BOT-BASE-BYFADOD1/commands/utility/enviarEmbed.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
+// BEM ESSE COMANDO ENVIA UMA EMBED FIXA OU SEJA PARA EDITAR SO NO CODIGO, O USO? DIVULGAR ALGO SLA , USE A CRIATIVIDADE
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('enviar_embed')
@@ -7,14 +7,14 @@ module.exports = {
 
     async execute(interaction) {
         const embed = new EmbedBuilder()
-            .setTitle('Acesse o link anexado para entrar no nosso canal oficial')
-            .setDescription('Esse e o canal onde postaremos as novidades e noticias sobre o desenvolvimento de nossos jogos , alem de fazermos conteudo relacionado a outros jogos e sobre o mundo de desenvolvimento de games!')
+            .setTitle('COLOQUE OQUE QUISER')
+            .setDescription('COLOQUE OQUE QUISER')
             .setColor(0x0099ff)
-            .setImage('https://i.postimg.cc/V6tkxGd6/Captura-de-tela-de-2024-05-17-12-25-43.png') // Substitua pela URL da sua imagem
-            .setThumbnail('https://i.postimg.cc/d3KVw2y9/Captura-de-tela-de-2024-05-17-12-06-12.png'); // Substitua pela URL da sua imagem
+            .setImage('IMAEM AQUI') // Substitua pela URL da sua imagem (RECOMENDO O POST IMG)
+            .setThumbnail('IMAGEM AQUI'); // Substitua pela URL da sua imagem (RECOMENDO O POST IMG)
 
         await interaction.reply({
-            content: 'Aqui está o [link do nosso canal oficial!](https://www.youtube.com/channel/UCwv2_xxrUCHziatF9UjsRSQ)', // Substitua pelo seu link
+            content: 'Aqui está o [link do nosso canal oficial!] LINK AQUI)', // Substitua pelo seu link
             embeds: [embed]
         });
     },
